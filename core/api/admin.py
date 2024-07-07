73d62c583c7b59fa2cc24141b83f5f075e415290
Valerolactone <varanchukv@gmail.com>
2024-07-08 01:02:01 +0300

    created database

---
+++ core/api/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import ProjectModel, TaskModel, ProjectParticipantsModel, TaskSubscribersModel, TasksAttachmentsModel
+
+
+@admin.register(ProjectModel)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskModel)
+class TaskAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(ProjectParticipantsModel)
+class ProjectParticipantsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TaskSubscribersModel)
+class TaskSubscribersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(TasksAttachmentsModel)
+class TasksAttachmentsModelAdmin(admin.ModelAdmin):
+    pass
